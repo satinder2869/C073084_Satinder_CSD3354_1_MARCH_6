86af4632b3a5d452dab71d2dada31ce60e11f488
visualstudio <visualstudio@230-PC38>
2019-03-07 15:16:02 -0500

    Lab 3

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -5,23 +5,12 @@ namespace C073084_Satinder_CSD3354_1_MARCH_6
 //Sourav Mehta C0730462
 //Assigmnment 2 
 //March 7,2019
-//Csd3354-1
+//Csd3354-1ssssss
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-
-            delegateExercises.Method2();
-        }
-    }
-
-
     public class DelegateExercises
     {
         public delegate void MyDelegate();
-        void Method1()
+        void Method1(int i)
         {
             Console.WriteLine("Method1");
             Console.ReadLine();

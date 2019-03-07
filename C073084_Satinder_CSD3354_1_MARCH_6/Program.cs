1b655cc3e72dc23ec2e894169338e9f1b96b1769
visualstudio <visualstudio@230-PC38>
2019-03-07 15:35:02 -0500

    Lab 6 Use the debugger to report on the results of running this code

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -7,26 +7,39 @@
 namespace C073084_Satinder_CSD3354_1_MARCH_6
 
 {
+
     public class Program
     {
         static void Main(string[] args)
         {
             DelegateExcercises delegateExcercises = new DelegateExcercises();
             delegateExcercises.Method3();
+            Console.ReadLine();
         }
     }
     public class DelegateExcercises
     {
-        public delegate int MyDelegate();
+        public delegate int MyDelegate(int intValue);
 
-        void Method1()
+        int Method1(int intMethod1)
         {
-            System.Console.WriteLine("MyDelegate");
+            return intMethod1 * 2;
+        }
+        int Method2(int intMethod1)
+        {
+            return intMethod1 * 10;
+        }
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
         }
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
     }
 }

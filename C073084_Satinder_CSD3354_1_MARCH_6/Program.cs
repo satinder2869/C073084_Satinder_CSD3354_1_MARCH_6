69472134bdfc45aaca04a4d59d0f6040b0e8e9af
visualstudio <visualstudio@230-PC38>
2019-03-07 13:29:51 -0500

    StaterCode

---
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C073084_Satinder_CSD3354_1_MARCH_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

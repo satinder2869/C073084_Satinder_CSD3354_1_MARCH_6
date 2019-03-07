24732528927bcb63b1a620d2e603860031ea5554
visualstudio <visualstudio@230-PC38>
2019-03-07 16:05:33 -0500

    Lab 14::Use the debugger to report the results of running this code

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -5,8 +5,8 @@
 //March 7,2019
 
 namespace C073084_Satinder_CSD3354_1_MARCH_6
-{
-    public class Program
+ {
+        public class Program
     {
         public static void Main()
         {
@@ -15,26 +15,23 @@ namespace C073084_Satinder_CSD3354_1_MARCH_6
             Console.ReadLine();
         }
     }
-
     public delegate int MyDelegate(out int i);
 
     public class DelegateExercises
     {
         int Method1(out int i)
         {
-            System.Console.WriteLine("Method1");
-            i = 10;
+            i = 100;
+            System.Console.WriteLine("Method1 " + i);
             return 0;
         }
-
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = null;
             MyDelegate myDelegate2 = myDelegate + myDelegate1;
             int intValue;
             myDelegate2(out intValue);
-
         }
     }
 

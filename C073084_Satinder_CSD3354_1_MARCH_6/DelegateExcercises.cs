ee66a9471ea6ac863025a897494b294093272d53
visualstudio <visualstudio@230-PC38>
2019-03-07 14:08:35 -0500

    Lab1: Working with Delegates Working

---
+++ C073084_Satinder_CSD3354_1_MARCH_6/DelegateExcercises.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C073084_Satinder_CSD3354_1_MARCH_6
+{
+    internal class DelegateExcercises
+    {
+        internal void Method2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 
 namespace C073084_Satinder_CSD3354_1_MARCH_6
+//Satinder Pal Singh C0730484
+//Sourav Mehta C0730462
+//Assigmnment 2 
+//March 7,2019
+//Csd3354-1
 {
-    public class DelegateExercises
+    public class Program
     {
         public delegate void MyDelegate();
-        void Methodl()
+
+        public void Method1()
         {
             Console.WriteLine("Method1");
             Console.ReadLine();
         }
-
         public void Method2()
         {
-            MyDelegate myDelegate = new MyDelegate(Methodl);
-            myDelegate();
+            MyDelegate mydelegate = new MyDelegate(Method1);
+            mydelegate();
+        }
+        static void Main(string[] args)
+        {
+
         }
     }
 }

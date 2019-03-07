cd005196024e1f02560ff0273b5ba23960a7a40e
visualstudio <visualstudio@230-PC38>
2019-03-07 16:10:20 -0500

    lab 15 : Use the debugger to report the results of running this code

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -5,33 +5,39 @@
 //March 7,2019
 
 namespace C073084_Satinder_CSD3354_1_MARCH_6
- {
-        public class Program
+{ 
+    public class Program
     {
         public static void Main()
         {
             DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
+            try
+            {
+                delegateExercises.Method3();
+                Console.ReadLine();
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine("Exception Occurred.");
+                Console.ReadLine();
+
+            }
         }
     }
-    public delegate int MyDelegate(out int i);
+
+    public delegate void MyDelegate();
 
     public class DelegateExercises
     {
-        int Method1(out int i)
+        void Method1()
         {
-            i = 100;
-            System.Console.WriteLine("Method1 " + i);
-            return 0;
+            throw new System.Exception();
         }
+
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = null;
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intValue;
-            myDelegate2(out intValue);
+            myDelegate();
         }
     }
 

c6cd65b0085dd37c1df23dc12b30cc6b7180413a
visualstudio <visualstudio@230-PC38>
2019-03-07 15:58:04 -0500

    Lab 12 : Use the debugger to report the results of running this code

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -6,14 +6,39 @@
 
 namespace C073084_Satinder_CSD3354_1_MARCH_6
 {
-    public class DelegateExercises
-    {
-        public object MyDelegate { get; private set; }
+    using System;
 
-        void Method3()
+    delegate void ExampleDelegate(string xyz);
+
+    class Program
+    {
+        public static void Method1(string xyz)
         {
-            Console.WriteLine(MyDelegate.ToString());
+            Console.WriteLine(xyz + " Method1");
+        }
+
+        public static void Method2(string xyz)
+        {
+            Console.WriteLine(xyz + " Method2");
+        }
+
+        public static void Main()
+        {
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            myDelegate = ex1Delegate - ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
+            Console.ReadLine();
         }
     }
 }
-

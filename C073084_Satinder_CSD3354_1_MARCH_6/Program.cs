69f18d1eee25f7cd48b9440dd8243f89ee8d3487
visualstudio <visualstudio@230-PC38>
2019-03-07 15:27:53 -0500

    lab 04

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -5,20 +5,35 @@ namespace C073084_Satinder_CSD3354_1_MARCH_6
 //Sourav Mehta C0730462
 //Assigmnment 2 
 //March 7,2019
-//Csd3354-1ssssss
 {
-    public class DelegateExercises
+    class Program
     {
-        public delegate void MyDelegate();
-        void Method1(int i)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Method1");
-            Console.ReadLine();
+            DelegateExcercises de = new DelegateExcercises();
+            de.Method3();
         }
-        public void Method2()
+    }
+    public class DelegateExcercises
+    {
+        public delegate int MyDelegate(int intValue);
+
+        public int Method1(int intMethod1)
+        {
+            return intMethod1 * 2;
+        }
+        public int Method2(int intMethod2)
+        {
+            return intMethod2 * 10;
+        }
+        public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            int result1 = myDelegate(10);
+            System.Console.WriteLine(result1);
+            myDelegate = new MyDelegate(Method2);
+            int result2 = myDelegate(10);
+            System.Console.WriteLine(result2);
         }
     }
 }

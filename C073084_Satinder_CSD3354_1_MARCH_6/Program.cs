b633ca72783540d79bdb0fda8632b3af29e55fcf
visualstudio <visualstudio@230-PC38>
2019-03-07 15:46:59 -0500

    Lab 10: Use the debugger to report on the result of running this code

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -5,11 +5,33 @@
 //March 7,2019
 
 namespace C073084_Satinder_CSD3354_1_MARCH_6
-
+{ 
+    public class Program
 {
-    public delegate void MyDelegate();
-    public class DelegateExcercises : MyDelegate
+    public static void Main()
     {
+        DelegateExercises delegateExercises = new DelegateExercises();
+        delegateExercises.Method3();
+        Console.ReadLine();
+    }
+}
+
+public delegate void MyDelegate();
+
+public class DelegateExercises
+{
+    void Method1()
+    {
+        System.Console.WriteLine("Method1");
+
 
     }
+
+    public void Method3()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        myDelegate();
+        System.Console.WriteLine(myDelegate.ToString());
+    }
+}
 }

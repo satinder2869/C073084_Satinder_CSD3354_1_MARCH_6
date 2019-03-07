fd1fe601522f83adf6481b49e83c0803719bdd83
visualstudio <visualstudio@230-PC38>
2019-03-07 15:32:43 -0500

    Lab 5: Use the  Debbuger to report on the results of the running this code

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -1,39 +1,32 @@
 ﻿using System;
-
-namespace C073084_Satinder_CSD3354_1_MARCH_6
 //Satinder Pal Singh C0730484
 //Sourav Mehta C0730462
 //Assigmnment 2 
 //March 7,2019
+
+namespace C073084_Satinder_CSD3354_1_MARCH_6
+
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            DelegateExcercises de = new DelegateExcercises();
-            de.Method3();
+            DelegateExcercises delegateExcercises = new DelegateExcercises();
+            delegateExcercises.Method3();
         }
     }
     public class DelegateExcercises
     {
-        public delegate int MyDelegate(int intValue);
+        public delegate int MyDelegate();
 
-        public int Method1(int intMethod1)
+        void Method1()
         {
-            return intMethod1 * 2;
-        }
-        public int Method2(int intMethod2)
-        {
-            return intMethod2 * 10;
+            System.Console.WriteLine("MyDelegate");
         }
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            int result1 = myDelegate(10);
-            System.Console.WriteLine(result1);
-            myDelegate = new MyDelegate(Method2);
-            int result2 = myDelegate(10);
-            System.Console.WriteLine(result2);
+            myDelegate();
         }
     }
 }

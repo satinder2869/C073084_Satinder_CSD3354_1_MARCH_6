b611adfdd10990ce0192da5f2271dcdc1b003fe4
visualstudio <visualstudio@230-PC38>
2019-03-07 15:37:20 -0500

    Lab 7: Use the debugger to report on the results of running this code

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -23,16 +23,16 @@ namespace C073084_Satinder_CSD3354_1_MARCH_6
 
         int Method1(int intMethod1)
         {
-            return intMethod1 * 2;
+            return intMethod1 * 4;
         }
         int Method2(int intMethod1)
         {
-            return intMethod1 * 10;
+            return intMethod1 * 20;
         }
         public void Method4(MyDelegate myDelegate)
         {
-            int result = myDelegate(10);
-            Console.WriteLine(result);
+            for (int i = 1; i <= 5; i++)
+                System.Console.Write(myDelegate(i) + " ");
         }
         public void Method3()
         {

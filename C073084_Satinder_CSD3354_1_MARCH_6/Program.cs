ad0f6d0883afb030acdc0cd0e58c5890f7198ccd
visualstudio <visualstudio@230-PC38>
2019-03-07 15:55:22 -0500

    Lab 11 Use the debugger to report the results of running this code

--- C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
+++ C073084_Satinder_CSD3354_1_MARCH_6/Program.cs
@@ -5,33 +5,15 @@
 //March 7,2019
 
 namespace C073084_Satinder_CSD3354_1_MARCH_6
-{ 
-    public class Program
 {
-    public static void Main()
+    public class DelegateExercises
     {
-        DelegateExercises delegateExercises = new DelegateExercises();
-        delegateExercises.Method3();
-        Console.ReadLine();
+        public object MyDelegate { get; private set; }
+
+        void Method3()
+        {
+            Console.WriteLine(MyDelegate.ToString());
+        }
     }
 }
 
-public delegate void MyDelegate();
-
-public class DelegateExercises
-{
-    void Method1()
-    {
-        System.Console.WriteLine("Method1");
-
-
-    }
-
-    public void Method3()
-    {
-        MyDelegate myDelegate = new MyDelegate(Method1);
-        myDelegate();
-        System.Console.WriteLine(myDelegate.ToString());
-    }
-}
-}
